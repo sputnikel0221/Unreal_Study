99aa6f4ee9aea283340b2c1f6f53d41ddd035cc1
sputnikel0221 <111990655+sputnikel0221@users.noreply.github.com>
2023-04-30 17:44:19 +0900

    Fountain actor asset Mapping
    with cpp

--- Source/Untitled/Fountain.cpp
+++ Source/Untitled/Fountain.cpp
@@ -29,10 +29,26 @@ AFountain::AFountain()
 	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		SM_BODY(TEXT("/Script/Engine.StaticMesh'/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01'"));
+		SM_BODY(TEXT("/Script/Engine.StaticMesh'/"
+			"Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/"
+			"SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01'"));
 
 	if (SM_BODY.Succeeded()) {
 		Body->SetStaticMesh(SM_BODY.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("/Script/Engine.StaticMesh'/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02'"));
+
+	if (SM_WATER.Succeeded()) {
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> //UParticleSystem
+		PS_SPLASH(TEXT("/Script/Engine.ParticleSystem'/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01'"));
+
+	if (PS_SPLASH.Succeeded()) {
+		Splash->SetTemplate(PS_SPLASH.Object); //SetTemplate
 	}
 }
 
