956053fc55571b6397e5cb8b2cdfc69d4657e682
sputnikel0221 <111990655+sputnikel0221@users.noreply.github.com>
2023-05-01 22:10:45 +0900

    Made GameMode / Pawn / Controller

---
+++ Source/Untitled/NewGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NewGameMode.h"
+#include "NewPawn.h" //pawn을 추가해줘야 함
+#include "NewPlayerController.h" //Controller추가
+
+
+ANewGameMode::ANewGameMode()
+{
+	DefaultPawnClass = ANewPawn::StaticClass();
+	PlayerControllerClass = ANewPlayerController::StaticClass();
+}
---
+++ Source/Untitled/NewGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+//#include "CoreMinimal.h"
+#include "Untitled.h" //Engine과 사용자LOG 사용하기 위함
+#include "GameFramework/GameModeBase.h"
+#include "NewGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLED_API ANewGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+		ANewGameMode();
+};
